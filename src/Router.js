20f92ecfb0badf2d5607ab4fb70aa54b321a04c0
regexowl <ksimicko@redhat.com>
2023-07-10 11:03:05 +0200

    build(deps-dev): Manually bump stylelint from 14.16.1 to 15.10.1
    The dependabot bump #1222 was failing with `ERESOLVE unable to resolve dependency tree`. Bumping styleling together with the upstream dependency resolves the problem.
    
    Bumps:
    - stylelint from 14.16.1 to 15.10.1
    - stylelint-config-recommended-scss from 7.0.0 to 12.0.0

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,10 @@
-import React, { lazy } from 'react';
+import React, { Suspense, lazy, useEffect, useState } from 'react';
 
+import { Bullseye, Spinner } from '@patternfly/react-core';
+import AsyncComponent from '@redhat-cloud-services/frontend-components/AsyncComponent';
+import ErrorState from '@redhat-cloud-services/frontend-components/ErrorState';
 import { useFlag } from '@unleash/proxy-client-react';
+import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 
 import EdgeImageDetail from './Components/edge/ImageDetails';
@@ -11,28 +15,66 @@ const LandingPage = lazy(() => import('./Components/LandingPage/LandingPage'));
 const CreateImageWizard = lazy(() =>
   import('./Components/CreateImageWizard/CreateImageWizard')
 );
+const INVENTORY_TOTAL_FETCH_URL = '/api/inventory/v1/hosts';
 
 export const Router = () => {
+  const [hasSystems, setHasSystems] = useState(true);
+
+  useEffect(() => {
+    try {
+      axios
+        .get(`${INVENTORY_TOTAL_FETCH_URL}?page=1&per_page=1`)
+        .then(({ data }) => {
+          setHasSystems(data.total > 0);
+        });
+    } catch (e) {
+      /*eslint-disable no-console*/
+      console.log(e);
+      /*eslint-enable no-console*/
+    }
+  }, [hasSystems]);
+
   const edgeParityFlag = useFlag('edgeParity.image-list');
   return (
-    <Routes>
-      <Route path="*" element={<LandingPage />}>
-        <Route path="imagewizard/:composeId?" element={<CreateImageWizard />} />
-        <Route path="share/:composeId" element={<ShareImageModal />} />
-      </Route>
+    <Suspense
+      fallback={
+        <Bullseye>
+          <Spinner size="xl" />
+        </Bullseye>
+      }
+    >
+      {!hasSystems ? (
+        <AsyncComponent
+          appName="dashboard"
+          module="./AppZeroState"
+          scope="dashboard"
+          ErrorComponent={<ErrorState />}
+          app="Images"
+        />
+      ) : (
+        <Routes>
+          <Route path="*" element={<LandingPage />}>
+            <Route
+              path="imagewizard/:composeId?"
+              element={<CreateImageWizard />}
+            />
+            <Route path="share/:composeId" element={<ShareImageModal />} />
+          </Route>
 
-      {edgeParityFlag && (
-        <Route
-          path={`/${manageEdgeImagesUrlName}/:imageId`}
-          element={<EdgeImageDetail />}
-        >
-          <Route path="*" element={<EdgeImageDetail />} />
-          <Route
-            path={`versions/:imageVersionId/*`}
-            element={<EdgeImageDetail />}
-          />
-        </Route>
+          {edgeParityFlag && (
+            <Route
+              path={`/${manageEdgeImagesUrlName}/:imageId`}
+              element={<EdgeImageDetail />}
+            >
+              <Route path="*" element={<EdgeImageDetail />} />
+              <Route
+                path={`versions/:imageVersionId/*`}
+                element={<EdgeImageDetail />}
+              />
+            </Route>
+          )}
+        </Routes>
       )}
-    </Routes>
+    </Suspense>
   );
 };
